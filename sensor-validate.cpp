540e9d382dc221bdb0a51ee0dc31e89c53c6c015
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-03-25 18:55:05 +0530

    Update sensor-validate.cpp

--- sensor-validate.cpp
+++ sensor-validate.cpp
@@ -1,28 +1,58 @@
 #include "sensor-validate.h"
-
-bool _give_me_a_good_name(double value, double nextValue, double maxDelta) {
-  if(nextValue - value > maxDelta) {
-    return false;
-  }
-  return true;
+/* =================================================================================
+* Function Name : IsSuddenJumpObserved
+* Description   : To check sudden jump
+* Arguments     : 1. value - list of value
+*		              2. numOfValues - Number of values
+*	                3. maxDelta - Maximum acceptable changes
+* Returns       : true or false
+* ================================================================================== */
+bool IsSuddenJumpObserved(double* value, double nextValue, double maxDelta) {
+  int lastButOneIndex = numOfValues - 1;
+	for(int i = 0; i < lastButOneIndex; i++) 
+	{
+		if((values[i + 1] - values[i]) > maxDelta) 
+		{
+			return false;
+		}
+	}
+	
+	return true;
 }
 
+/* ==================================================================================
+* Function Name : validateSOCreadings
+* Description   : validate the SOC input values
+* Arguments     : 1. values - list of SOC values
+*		              2. numOfValues - Number of SOC values
+*  Returns       : true or false
+* ================================================================================== */
 bool validateSOCreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
-      return false;
-    }
-  }
-  return true;
+  
+ if(values != NULL)
+	{
+		if(!IsSuddenJumpObserved(values, numOfValues, 0.05)) 
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
+/* ==================================================================================
+* Function Name : validateCurrentreadings
+* Description   : validate the Current readings and check  null pointer
+* Arguments	    : 1. values - list of Current values
+*		              2. numOfValues - Number of Current values
+* Returns       : true or false
+* ================================================================================== */
 bool validateCurrentreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
-      return false;
-    }
-  }
-  return true;
+  if(values != NULL)
+	{
+		if(!IsSuddenJumpObserved(values, numOfValues, 0.01)) 
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
