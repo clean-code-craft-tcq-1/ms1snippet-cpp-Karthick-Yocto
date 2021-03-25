a05db67da139dc0c4ae58ce08ecaed3acd770ea3
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-03-25 17:56:38 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -3,15 +3,20 @@
 #include "test/catch.hpp"
 #include "sensor-validate.h"
 
-TEST_CASE("reports error when soc jumps abruptly") {
+TEST_CASE("Reports error when soc jumps abruptly") {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
   REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == false);
 }
 
-TEST_CASE("reports error when current jumps abruptly") {
+TEST_CASE("Reports error when current jumps abruptly") {
   double currentReadings[] = {0.03, 0.03, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateSOCreadings(currentReadings, numOfCurReadings) == false);
 }
+
+TEST_CASE("Check for NULL pointer") {
+  double* currentReadings = NULL;
+  int numOfCurReadings = 0;
+  REQUIRE(validateCurrentreadings(currentReadings, numOfCurReadings, Normal) == true);
 
